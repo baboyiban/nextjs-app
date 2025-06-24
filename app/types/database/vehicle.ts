7f0b18ff9cf1ce76016ea95b1bfc831057e7f92e
최다루한 <chl11wq12@gmail.com>
2025-06-24 18:07:34 +0900

    fix: vehicleSchema enum->string

--- app/types/database/vehicle.ts
+++ app/types/database/vehicle.ts
@@ -5,7 +5,7 @@ export const VehicleSchema = z.object({
   vehicle_id: z.string(),
   current_load: z.number(),
   max_load: z.number(),
-  led_status: z.enum(["초록", "하양", "빨강"]).optional(),
+  led_status: z.string(),
   needs_confirmation: z.boolean(),
   coord_x: z.number(),
   coord_y: z.number(),
